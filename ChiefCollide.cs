1f26bb8bc12e50acd1044b4c99f5965870603e9c
Ike Muoma <Ikemuoma70@gmail.com>
2022-04-09 20:36:06 +0100

    First version of ChiefCollide

---
+++ ChiefCollide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChiefCollide : MonoBehaviour
+{
+
+    public AudioSource evillaugh;
+    public AudioSource injuredBerry;
+    //if you colide with Myan Chief, zap health
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "MayanChief")
+        {
+            if (!evillaugh.isPlaying)
+            {
+                evillaugh.Play();
+            }
+            if (!injuredBerry.isPlaying)
+            {
+                injuredBerry.Play();
+            }
+
+            SceneController.MyHealth = SceneController.MyHealth - 10;
+            if (SceneController.MyHealth < 0)
+                SceneController.timeout = true;
+        }
+    }
+}
