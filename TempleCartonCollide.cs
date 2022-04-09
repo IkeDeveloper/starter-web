27297dc29651ae4fa756ba80e1d44b8d365fb8bf
Ike Muoma <Ikemuoma70@gmail.com>
2022-04-09 21:16:42 +0100

    First version of TempleCartonCollide

---
+++ TempleCartonCollide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TempleCartonCollide : MonoBehaviour
+{
+   
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "TempleCarton")
+        {
+            SceneController.pyramidCartonCollected = true;
+            SceneController.score = SceneController.score + 50;
+        }
+
+    }
+}
