2ca4aa37e72ac5ddaa92e16c852e2a640df3514d
Ike Muoma <Ikemuoma70@gmail.com>
2022-04-09 20:24:08 +0100

    First version of CartonColide

---
+++ CartonCollide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Berry collides with Ribina Carton
+public class CartonCollide : MonoBehaviour
+{
+    public AudioSource gotcarton;
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Carton")
+        {
+            gotcarton.Play();
+            SceneController.cartonsCollected = SceneController.cartonsCollected + 1;
+            SceneController.MyHealth = SceneController.MyHealth + 5;
+            SceneController.score = SceneController.score + 20;
+
+            Destroy(col.gameObject);
+        }
+         
+    }
+}
